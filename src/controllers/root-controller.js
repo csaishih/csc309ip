127263ca89491eeba45d7b972300d8f3c2d9dfde
Shihan Ai <shihan.ai@mail.utoronto.ca>
2015-03-27 20:07:03 -0400

    8

--- src/controllers/root-controller.js
+++ src/controllers/root-controller.js
@@ -1,150 +1,108 @@
-var root = angular.module('root', ['angularModalService', 'ngAnimate', 'toastr']);
-root.controller('RootController', ['$scope', '$http', 'ModalService', '$window', 'toastr', function($scope, $http, ModalService, $window, toastr) {
+var app = angular.module('root', ['ui.bootstrap', 'ngAnimate', 'toastr']);
+app.controller('RootController', function($scope, $modal, $http, $window) {
 	$scope.login = function() {
-		ModalService.showModal({
-			templateUrl: '/src/html/modal_login.html',
+		var modalInstance = $modal.open({
+			templateUrl: 'modal_login.html',
 			controller: 'LoginModalController'
-		}).then(function(modal) {
-			modal.element.modal();
-			modal.close.then(function(result) {
-				if (Object.keys(result).length == 0 || result.email == '' || result.password == '') {
-					console.log("Error");
+		});
+
+		modalInstance.result.then(function(result) {
+			var data = JSON.stringify(eval("({ email: '" + result.email + "', password: '" + result.password + "' })"));
+			$http.post('/login', data).success(function(response) {
+				if (response) {
+					$window.location.href = '/';
 				} else {
-					var data = JSON.stringify(eval("({ email: '" + result.email + "', password: '" + result.password + "' })"));
-					$http.post('/login', data).success(function(response) {
-						if (response) {
-							$window.location.href = '/';
-						} else {
-							console.log("Error");
-							toastr.error('Error', 'Invalid email or password', {closeButton: true});
-							$scope.login();
-						}
-					});
+					toastr.error('Invalid email or password', 'Error');
+					$scope.login();
 				}
 			});
 		});
 	};
 
 	$scope.signup = function() {
-		ModalService.showModal({
-			templateUrl: '/src/html/modal_signup.html',
+		var modalInstance = $modal.open({
+			templateUrl: 'modal_signup.html',
 			controller: 'SignupModalController'
-		}).then(function(modal) {
-			modal.element.modal();
-			modal.close.then(function(result) {
-				if (Object.keys(result).length == 0) {
-					console.log("Error");
+		});
+
+		modalInstance.result.then(function(result) {
+			var data = JSON.stringify(eval("({ name: '" + result.name + "', email: '" + result.email + "', password: '" + result.password + "' })"));
+			$http.post('signup', data).success(function(response) {
+				if (response) {
+					$scope.login();
 				} else {
-					var data = JSON.stringify(eval("({ name: '" + result.name + "', email: '" + result.email + "', password: '" + result.password + "' })"));
-					$http.post('signup', data).success(function(response) {
-						if (response) {
-							$scope.login();
-						} else {
-							console.log("Cancel");
-						}
-					});
+					toastr.error('The email address you entered is already taken', 'Error');
 				}
 			});
-		});
+		});	
 	};
+});
 
-
-}]);
-
-root.controller('LoginModalController', ['$scope', '$element', 'toastr', 'close', function($scope, $element, toastr, close) {
+app.controller('LoginModalController', function($scope, $modalInstance, toastr) {
 	$scope.submit = function() {
-		close({
-			email: $scope.email,
-			password: $scope.password
-		}, 500);
+		console.log($scope.password);
+		if ($scope.checkError($scope.email)) {
+			toastr.error('Please enter a valid email', 'Error');
+		} else if ($scope.checkError($scope.password)) {
+			toastr.error('Please enter your password', 'Error');
+		} else {
+			$modalInstance.close({
+				email: $scope.email,
+				password: $scope.password
+			});
+		}
 	};
+
+	$scope.checkError = function(object){
+		return (object === undefined || object === '');
+	}
 
 	$scope.cancel = function() {
-		$element.modal('hide');
-		close({}, 500);
+		$modalInstance.dismiss('cancel');
 	};
-}]);
+});
 
-root.controller('SignupModalController', ['$scope', '$element', 'toastr', 'close', function($scope, $element, toastr, close) {
-	$scope.checkErrorName = function() {
-		if ($scope.name == '' || $scope.name == undefined) {
-			toastr.error('Please enter your name', 'Error', {
-				"closeButton": true,
-				"hideDuration": "3000",
-				"timeOut": "2500"
-			});
-			return true;
-		} else {
-			return false;
-		}
-	};
-
-	$scope.checkErrorEmail = function() {
-		if ($scope.email == '' || $scope.email == undefined) {
-			toastr.error('Please enter a valid email', 'Error', {
-				"closeButton": true,
-				"hideDuration": "3000",
-				"timeOut": "2500"
-			});
-			return true;
-		} else {
-			return false;
-		}		
-	};
-
-	$scope.checkErrorPassword = function() {
-		if ($scope.password == '' || $scope.password == undefined) {
-			toastr.error('Please enter your password', 'Error', {
-				"closeButton": true,
-				"hideDuration": "3000",
-				"timeOut": "2500"
-			});
-			return true;
-		} else {
-			return false;
-		}		
-	};
-
-	$scope.checkErrorRepassword = function() {
-		if ($scope.repassword == '' || $scope.repassword == undefined) {
-			toastr.error('Please re-enter your password', 'Error', {
-				"closeButton": true,
-				"hideDuration": "3000",
-				"timeOut": "2500"
-			});
-			return true;
-		} else {
-			return false;
-		}
-	};
-
+app.controller('SignupModalController', function($scope, $modalInstance, toastr) {
 	$scope.submit = function() {
-		if (!$scope.checkErrorName() && !$scope.checkErrorEmail() && !$scope.checkErrorPassword() && !$scope.checkErrorRepassword() && $scope.checkPassword($scope.password, $scope.repassword)) {
-			console.log("pass");
-			close({
+		if ($scope.checkError($scope.name)) {
+			toastr.error('Please enter your full name', 'Error');
+		} else if ($scope.checkError($scope.email)) {
+			toastr.error('Please enter a valid email', 'Error');
+		} else if ($scope.checkError($scope.password)) {
+			toastr.error('Please enter your password', 'Error');
+		} else if ($scope.checkError($scope.repassword)) {
+			toastr.error('Please re-enter your password', 'Error');
+		} else if ($scope.checkPassword($scope.password, $scope.repassword)) {
+			$modalInstance.close({
 				name: $scope.name,
 				email: $scope.email,
 				password: $scope.password,
 				repassword: $scope.repassword
-			}, 500);
+			});
 		}
-	};
-
-	$scope.cancel = function() {
-		$element.modal('hide');
-		close({}, 500);
 	};
 
 	$scope.checkPassword = function(password, repassword) {
 		//Should check more than this
 		if (password != repassword) {
-			toastr.error('Passwords do not match!', 'Error', {
-				"closeButton": true,
-				"hideDuration": "3000",
-				"timeOut": "2500"
-			});
-			return false;
+			toastr.error('Passwords do not match!', 'Error');
 		}
 		return password == repassword;
 	}
-}]);
+
+	$scope.checkError = function(object){
+		return (object === undefined || object === '');
+	}
+
+	$scope.cancel = function() {
+		$modalInstance.dismiss('cancel');
+	};
+});
+
+app.config(function(toastrConfig) {
+	angular.extend(toastrConfig, {
+		closeButton: true,
+		maxOpened: 1,
+		timeOut: 2500
+	});
+});
